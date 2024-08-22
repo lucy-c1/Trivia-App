92cb911dbed8613356b74b1763aa94e51ee3de08
LucyC-1225 <lucyc1225xx@gmail.com>
2024-08-22 10:20:12 -0400

    References #2 - added styles to cover page

--- CoverPage.jsx
+++ CoverPage.jsx
@@ -2,17 +2,17 @@ import React from "react"
 
 export default function CoverPage() {
     return (
-        <div>
+        <div className = "page-container">
             <div className = "coverpage-container">
                 <div className = "title-container">
                     <h1 class = "h1-style">Quizz</h1>
                     <h3 class = "h3-style">Test your knowledge across a wide range of topics!</h3>
                 </div>
-                <h2 className = "h2-style">Configure your quiz:</h2>
+                <h2 className = "h2-style" id = "configure-title">Configure your quiz:</h2>
                 <form className = "form-container">
-                    <div className="form-group">
-                        <label htmlFor="form-select">Choose a category:</label>
-                        <select class="form-control" id = "form-select" name = "category">
+                    <div className="form-group" id = "group1">
+                        <label htmlFor="form-select" className = "h3-style">Choose a category:</label>
+                        <select class="form-control form-control-style" id = "form-select" name = "category">
                             <option value="Any category">Any category</option>
                             <option value="Books">Books</option>
                             <option value="Film">Film</option>
@@ -20,11 +20,11 @@ export default function CoverPage() {
                     </div>
 
                     <div className="form-group">
-                        <label htmlFor="number-input">Number of questions:</label>
-                        <input type="number" className = "form-control" id = "number-input" name = "numQuestions" defaultValue = "5" />
+                        <label htmlFor="number-input" className = "h3-style">Number of questions:</label>
+                        <input type="number" className = "form-control form-control-style" id = "number-input" name = "numQuestions" defaultValue = "5" />
                     </div>
 
-                    <button type="submit" className = "submit-button">Start Quiz</button>
+                    <button type="submit" className = "button-style" id = "start-quiz-button">Start Quiz</button>
                 </form>
             </div>
         </div>
